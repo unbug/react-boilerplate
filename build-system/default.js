967e6f4b7623003e29f4ad1e9d175a2a068baa1c
unbug <tidelgl@gmail.com>
2019-08-29 14:50:04 +0800

    Update default.js
    update watch task

--- build-system/default.js
+++ build-system/default.js
@@ -3,16 +3,18 @@
 const gulp = require('gulp');
 const $ = require('./util');
 
-function watch(files, cb) {
-  return gulp.watch(files, event => {
-    $.util.log($.util.colors.bold('File ' + event.path + ' was ' + event.type + ', running tasks...'));
-    cb();
+function watch(files, task) {
+  const watcher = gulp.watch(files, task);
+  ['add', 'addDir', 'change', 'unlink', 'unlinkDir'].forEach(type => {
+    watcher.on(type, (path, stats) => {
+      $.util.log($.util.colors.bold('File ' + path + ' was ' + type + ', running tasks...'));
+    });
   });
 }
 
 // Watches for changes in files.
 gulp.task('watch', cb => {
-  watch(['lib.config.js'], () => gulp.series('lint', 'build:extra', 'build:lib-js', 'build:lib-css'));
-  watch(['styles/**/*.*'], () => gulp.series('lint', 'build:app-css'));
+  watch(['lib.config.js'], gulp.series('lint', 'build:extra', 'build:lib-js', 'build:lib-css'));
+  watch(['styles/**/*.*'], gulp.series('lint', 'build:app-css'));
   cb();
 });
